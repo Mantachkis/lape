4b563043a84534d181de16b5a310adedcba78e23
Mantachkis <82802203+Mantachkis@users.noreply.github.com>
2021-08-11 21:59:33 +0300

    style

--- bankasNaujas/view/list.php
+++ bankasNaujas/view/list.php
@@ -6,7 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List</title>
+    <style>
+    div {
+        margin: 5px;
+        padding: 7px;
+        border: 1px solid #ccc;
+        font-size: 25px;
+        display: inline-block;
+    }
+
+    button {
+        width: 200px;
+        margin: 10px;
+    }
+    </style>
 </head>
+
 
 <body>
 
@@ -14,23 +29,24 @@
     foreach ($bank as $key => $i) { ?>
     <form action="" method="GET">
         <div>
-            <?php echo ($bank[$key]['acc']), '  ', ($bank[$key]['surname']); ?>
-            <button type="submit" name="route" value="del_acc">Ištrinti</button>
+            <?php echo ($bank[$key]['acc']); ?>
+            <button type="submit" name="route" value="del_acc">Delete</button>
             <input type="hidden" name="id" value="<?= $bank[$key]['id'] ?>">
-        </div>
+
     </form>
 
-    <form action="<?= URL ?>?route=addFundsPage" method="POST">
+    <form style="display:inline" action="<?= URL ?>?route=addFundsPage" method="POST">
         <input type="hidden" name="id" value="<?= $bank[$key]['id'] ?>">
         <input type="hidden" name="route" value="addFundsPage">
         <button type="submit" value="addFunds">Add Funds</button>
     </form>
 
-    <form action="<?= URL ?>?route=deductFundsPage" method="POST">
+    <form style="display:inline" action="<?= URL ?>?route=deductFundsPage" method="POST">
         <input type="hidden" name="id" value="<?= $bank[$key]['id'] ?>">
         <input type="hidden" name="route" value="deductFundsPage">
         <button type="submit" value="deductFundsPage">Deduct Funds</button>
     </form>
+    </div>
 
     <?php }
     ?>
--- bankasNaujas/view/pirmas.php
+++ bankasNaujas/view/pirmas.php
@@ -6,21 +6,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bank</title>
+    <style>
+        nav {
+            background: gray;
+            margin: 10px 5px;
+            padding: 5px;
+        }
+
+        nav a {
+            display: inline-block;
+            margin: 6px;
+            font-size: 20px;
+            text-decoration: none;
+            color: black;
+        }
+    </style>
 </head>
 
 <body>
 
+    <nav>
 
-    <form action="" method="GET">
-        <input type="hidden" name="route" value="list">
-        <button type="submit">Account list</button>
-    </form>
-    <form action="" method="GET">
-        <input type="hidden" name="route" value="add">
-        <button type="submit">Add Account</button>
-    </form>
+        <a href="<?= URL ?>?route=list">Get list</button>
+
+            <a href="<?= URL ?>?route=add">Add account</button>
 
 
+    </nav>
 
 </body>
 
